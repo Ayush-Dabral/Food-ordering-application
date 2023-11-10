1afc204d2f56978193e043ef2d68f07c77271e2b
Ayush Dabral <ayush.dabral02@gmail.com>
2023-11-11 02:28:02 +0530

    Two pages developed

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    backgroundImage: {
+      'bgImg': "url('/Images/bgImg.png')",
+    },
+    fontFamily: {
+      'Pop': ['Poppins']
+    },
+    screens: {
+      'sm': '480px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      colors: {
+        'titles': '#192F60',
+        'para': '#8C96AB',
+      },
+      
+    },
+  },
+  plugins: [],
+}
+
